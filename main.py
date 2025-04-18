41b8d8ba6355260a610ec77367646c241b86249f
ellariell <evgenijvns0711@gmail.com>
2025-04-18 14:56:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import asyncio
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
-API_TOKEN = "твой_токен"  # Замени на настоящий токен
+API_TOKEN = "7648494160:AAFxkHe-E9-1revY1tMGM1gVFz92L6zaXKI"
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+
+@dp.message()
+async def handle_message(message: types.Message):
+    user_id = message.chat.id
+    msg = message.text
+    await bot.send_message(chat_id=user_id, text=msg + '\nОтвет: "Выполнено"')
+
 
 async def main():
-    user_id = 123456789  # Заменить на реальный user_id
-    msg = "Пример сообщения"
-    await bot.send_message(chat_id=user_id, text=msg + '\nОтвет: "Выполнено"')
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
