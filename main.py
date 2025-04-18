4ee85328e444dd1f301080d3d36d9a6d74f405f2
ellariell <evgenijvns0711@gmail.com>
2025-04-18 13:21:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, Router
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.types import Message
-from aiogram.utils import executor
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,15 +14,21 @@ if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is not set. Убедитесь, что токен задан в переменных окружения или в коде.")
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher(bot)
+dp = Dispatcher()
+router = Router()
 
-@dp.message_handler(commands=["start"])
+@router.message(commands=["start"])
 async def send_welcome(message: Message):
-    await message.reply("Привет! Я твой бот-напоминалка. Готов к работе!")
+    await message.answer("Привет! Я твой бот-напоминалка. Готов к работе!")
 
-@dp.message_handler()
+@router.message()
 async def echo(message: Message):
-    await message.reply(f"Ты сказал: {message.text}")
+    await message.answer(f"Ты сказал: {message.text}")
+
+dp.include_router(router)
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
