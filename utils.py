e8734bab7a6a713fe390387eeacbc29f4a58b4ef
ellariell <evgenijvns0711@gmail.com>
2025-04-18 21:44:34 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def main_menu_keyboard():
-    return ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text="📋 Меню")]],
-        resize_keyboard=True,
-        is_persistent=True
-    )
