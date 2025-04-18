833e465591024632fd20f77e9f879c458d1229ad
ellariell <evgenijvns0711@gmail.com>
2025-04-18 21:43:39 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "supersecret")
-WEBHOOK_URL = os.getenv("RENDER_EXTERNAL_URL", "") + WEBHOOK_PATH
-
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = int(os.getenv("PORT", 8000))
